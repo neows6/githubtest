91d27c3dca150ce1ec1c9675f58e67585c5307eb
DJEagleOlsen <neows6@gmail.com>
2026-05-02 09:58:53 -0400

    Replace component with Tri-Model Prediction System

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import MegaMillionsFluid from "../mega-millions-fluid.jsx";
+import TriModelPredictor from "../mega-millions-fluid.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <MegaMillionsFluid />
+    <TriModelPredictor />
   </React.StrictMode>
 );
